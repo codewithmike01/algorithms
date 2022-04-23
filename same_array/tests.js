ebd1ec0808cdee551a5cab9950a2730c3eeca6c1
ginohmk <michotall95@gmail.com>
2022-04-23 08:55:36 +0100

    Add test

--- same_array/tests.js
+++ same_array/tests.js
@@ -9,4 +9,8 @@ describe('Same Array', () => {
   it('[2, 4, 1, 5] and [4, 1, 25]', () => {
     assert(same([2, 4, 1, 5], [4, 1, 25]) === false);
   });
+
+  it('[4] and [16]', () => {
+    assert(same([2, 4, 1, 5], [4, 1, 25]) === false);
+  });
 });
