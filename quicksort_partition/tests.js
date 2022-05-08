447981fafd084072c1de6020f5459eec4ade6adf
ginohmk <michotall95@gmail.com>
2022-05-08 16:05:29 +0100

    Add test to quick sort partition algorithm

--- quicksort_partition/tests.js
+++ quicksort_partition/tests.js
@@ -14,4 +14,19 @@ describe('Partition Quicksort', () => {
     let arr = [4, 5, 3, 9, 1];
     assert(JSON.stringify(partition(arr)) == JSON.stringify([3, 1, 4, 5, 9]));
   });
+
+  it('[15, 3, 9, 1, 6, 8, 19, 4, 20]', () => {
+    let arr = [15, 3, 9, 1, 6, 8, 19, 4, 20];
+    assert(
+      JSON.stringify(partition(arr)) ==
+        JSON.stringify([3, 9, 1, 6, 8, 4, 15, 19, 20])
+    );
+  });
+
+  it('[1, 5, 3, 6, 3, 9]', () => {
+    let arr = [1, 5, 3, 6, 3, 9];
+    assert(
+      JSON.stringify(partition(arr)) == JSON.stringify([1, 5, 3, 6, 3, 9])
+    );
+  });
 });
